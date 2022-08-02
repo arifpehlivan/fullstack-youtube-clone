565b8616c380ff1c1ca9e61d72c4dcf444692fb0
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-02 04:25:34 +0300

    add crud video

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from "../controllers/user.js"
-import { verifyToken } from "../verifyToken.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
