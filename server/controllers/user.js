a2e2b8c4908b648afe578c87b3cba3deede57b6d
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-02 03:23:13 +0300

    add get subscribe unsubscribe

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -30,17 +30,50 @@ export const deleteUser = async (req,res,next) => {
     }
 }
 export const getUser = async (req,res,next) => {
-    
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch (err) {
+        next(err)
+    }
 }
 export const subscribe = async (req,res,next) => {
-    
+    try {
+        await User.findById(req.user.id,{
+            $push:{subscribedUsers:req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id,{
+            $inc:{subscribers:1}
+        })
+        res.status(200).json("Subscription succesfull.")
+    } catch (err) {
+        next(err)
+    }
 }
 export const unsubscribe = async (req,res,next) => {
-    
+    try {
+        await User.findById(req.user.id,{
+            $pull:{subscribedUsers:req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id,{
+            $inc:{subscribers:-1}
+        })
+        res.status(200).json("Unsubscription succesfull.")
+    } catch (err) {
+        next(err)
+    }
 }
 export const like = async (req,res,next) => {
-    
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
 }
 export const dislike = async (req,res,next) => {
-    
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
 }
