4a422062441ede573ffabd872aa4bc5cbfc900dd
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-02 19:05:19 +0300

    add search tags

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -96,7 +96,7 @@ export const getByTag = async (req,res,next) => {
     const tags = req.query.tags.split(",")
     console.log(tags);
     try {
-        const videos = await Video.find().sort({views:-1})
+        const videos = await Video.find({tags: {$in: tags}}).limit(20);
         res.status(200).json(videos)
     } catch (err) {
         next(err)
