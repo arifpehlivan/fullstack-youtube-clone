a4d8e6c4f7c4b84b8e686e3b5dea3794764b7cf9
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-03 03:35:54 +0300

    add menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
+import styled from "styled-components"
+import { Menu } from "./components/Menu";
+import { Navbar } from "./components/Navbar";
 
+const Container = styled.div`
+  display: flex;
+  padding: 0px;
+  margin: 0px;
+`
+const Main = styled.div`
+  flex:7
+`
+const Wrapper = styled.div`
+
+`
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Container>
+      <Menu/>
+      <Main>
+        <Navbar/>
+          <Wrapper>
+            test
+          </Wrapper>
+      </Main>
+    </Container>
   );
 }
 
