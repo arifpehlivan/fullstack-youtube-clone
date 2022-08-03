603846c7fc48987755092c5ed16f5f9888606ce2
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-03 18:19:31 +0300

    add dark light mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import styled from "styled-components"
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components"
 import { Menu } from "./components/Menu";
 import { Navbar } from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme.js";
 
 const Container = styled.div`
   display: flex;
@@ -8,23 +10,61 @@ const Container = styled.div`
   margin: 0px;
 `
 const Main = styled.div`
-  flex:7
+  flex:7;
+  background: ${({ theme }) => theme.bg}; 
 `
 const Wrapper = styled.div`
 
 `
 
 function App() {
+  const [darkMode,setDarkMode] = useState(false);
   return (
-    <Container>
-      <Menu/>
-      <Main>
-        <Navbar/>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode}/>
+        <Main>
+          <Navbar />
           <Wrapper>
-            test
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            
           </Wrapper>
-      </Main>
-    </Container>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
